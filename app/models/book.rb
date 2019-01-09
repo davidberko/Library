e365c49c87b4a326c8030332a3e1550624e1b79c
davidberko <davidmichaelberko@gmail.com>
2019-01-09 16:50:23 -0600

    added date for when books are available to check out again

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,19 @@
 class Book < ApplicationRecord
   has_many :checkouts
-  has_many :users, :through => :checkouts 
+  has_many :users, :through => :checkouts
+
+  def checked_out?
+    if self.checkouts == nil
+      print = "still available"
+    else
+      print = "already checked out"
+      print = "it will be available again on #{self.available}"
+    end
+  end
+
+  def available
+    if self.checkouts != nil
+      print = "#{self.checkouts.pluck(:due_date)}"
+    end
+  end
 end
